ee71ab949aa9c8c30c8b42ea83b3f9afbded079e
Gabriel Núñez <gnunez88@protonmail.com>
2022-06-08 15:52:52 -0400

    v0.4: more simple functions added, aliases, checks for PATH and separating non-local variables into .nonroam file

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Backing up old files/directories
 cp -Rf $(realpath $(dirname $0)/vim) ${HOME}/.vim
-cp -f $(realpath $(dirname $0)/inputrc) ${HOME}/.inputrc
-cp -f $(realpath $(dirname $0)/tmux.conf) ${HOME}/.tmux.conf
-cp -f $(realpath $(dirname $0)/envrc) ${HOME}/.envrc
+cp  -f $(realpath $(dirname $0)/inputrc) ${HOME}/.inputrc
+cp  -f $(realpath $(dirname $0)/tmux.conf) ${HOME}/.tmux.conf
+cp  -f $(realpath $(dirname $0)/envrc) ${HOME}/.envrc
 
+# Detecting wether bash is used or zsh
 if [ "${SHELL##*/}" = "zsh" ]; then
     SHELLRC="${HOME}/.zshrc"
     DETECTED="y"
