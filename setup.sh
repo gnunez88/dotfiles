f4be1188488fccfc2d6f9567703375178365afaa
Gabriel Núñez <gnunez88@protonmail.com>
2022-07-23 08:40:32 -0400

    v0.8: fixing missing backup directory for Vim; increasing the tmux history-limit option; adding a function to turn off the NEWLINE_BEFORE_PROMPT option when using ZSH

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Customising settings
+function no_newline_before_prompt () {
+    TARGET_FILE="${HOME}/.zshrc"
+    sed -ri '/^NEWLINE_BEFORE_PROMPT/s/yes/no/' ${TARGET_FILE}
+}
+
 # Backing up old files/directories
 cp -Rf $(realpath $(dirname $0)/vim) ${HOME}/.vim
 cp  -f $(realpath $(dirname $0)/inputrc) ${HOME}/.inputrc
@@ -10,6 +16,7 @@ cp  -f $(realpath $(dirname $0)/envrc) ${HOME}/.envrc
 if [ "${SHELL##*/}" = "zsh" ]; then
     SHELLRC="${HOME}/.zshrc"
     DETECTED="y"
+    no_newline_before_prompt
 elif [ "${SHELL##*/}" = "bash" ]; then
     SHELLRC="${HOME}/.bashrc"
     DETECTED="y"
